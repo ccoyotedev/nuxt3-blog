975dd87b5f8e2fd8b21076ae9072b0adf585a443
Caleb Brown <44173285+cgcbrown@users.noreply.github.com>
2022-05-08 14:21:17 +0100

    Organised components:

--- layouts/animation.vue
+++ layouts/animation.vue
@@ -7,7 +7,7 @@
 <style scoped>
 .ani-content {
   opacity: 1;
-  transition: opacity 500ms, transform 500ms;
+  transition: opacity 500ms, transform 400ms;
   padding:  calc(8.333% + 5.4rem) 8.333%;
   height: 100%;
 }
