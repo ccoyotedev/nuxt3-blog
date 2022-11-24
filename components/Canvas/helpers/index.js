22db687d580b59496dcda80388cde66e83fa4ece
Caleb Brown <44173285+cgcbrown@users.noreply.github.com>
2022-11-24 21:51:55 +0000

    feat: ship added

---
+++ components/Canvas/helpers/index.js
@@ -0,0 +1,47 @@
+const PRIMARY_COLOR = "#02dc81";
+
+export function drawLine(ctx, {
+  x1,
+  y1
+}, {
+  x2,
+  y2
+}) {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.strokeStyle = PRIMARY_COLOR;
+  ctx.stroke();
+};
+
+export function drawCircle(ctx, x, y, radius) {
+  ctx.beginPath();
+  ctx.arc(
+    x,
+    y,
+    radius,
+    0,
+    2 * Math.PI,
+    false
+  );
+  ctx.strokeStyle = PRIMARY_COLOR;
+  ctx.stroke();
+}
+
+export function coordinateOfPointAfterRotation(x, y, radians) {
+  const x1 = x * Math.cos(radians) - y * Math.sin(radians)
+  const y1 = y * Math.cos(radians) + x * Math.sin(radians)
+  return {
+    x: x1,
+    y: y1
+  };
+}
+
+export function calculateXYDisplacement(theta, hyp) {
+  const dx = hyp * Math.cos(theta);
+  const dy = hyp * Math.sin(theta);
+  return {
+    dx,
+    dy
+  };
+}
--- components/Canvas/index.vue
+++ components/Canvas/index.vue
@@ -3,12 +3,26 @@
 </template>
 
 <script>
+import {
+  drawLine,
+  drawCircle,
+  coordinateOfPointAfterRotation,
+} from "./helpers/index.js";
+
 export default {
   name: "Canvas",
   data() {
     return {
-      mouseX: 0,
-      mouseY: 0,
+      mouse: {
+        x: 0,
+        y: 0,
+      },
+      ship: {
+        x: 300,
+        y: 300,
+        height: 40,
+        width: 30,
+      },
     };
   },
   mounted() {
@@ -43,19 +57,79 @@ export default {
       requestAnimationFrame(this.animate);
 
       this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-      this.draw();
+      this.drawPointer();
+      this.drawShip();
     },
     trackMouse(e) {
       const rect = this.canvas.getBoundingClientRect();
-      this.mouseX = e.x - rect.left;
-      this.mouseY = e.y - rect.top;
+      this.mouse.x = e.x - rect.left;
+      this.mouse.y = e.y - rect.top;
     },
-    draw() {
-      this.ctx.beginPath();
-      this.ctx.arc(this.mouseX, this.mouseY, 5, 0, 2 * Math.PI, false);
-      this.ctx.fillStyle = "#02dc81";
-      this.ctx.fill();
-      this.ctx.stroke();
+    drawPointer() {
+      const { x, y } = this.mouse;
+      const radius = 6;
+      drawCircle(this.ctx, this.mouse.x, this.mouse.y, radius);
+      drawLine(
+        this.ctx,
+        { x1: x - (5 * radius) / 3, y1: y },
+        { x2: x - radius / 3, y2: y }
+      );
+      drawLine(
+        this.ctx,
+        { x1: x + (5 * radius) / 3, y1: y },
+        { x2: x + radius / 3, y2: y }
+      );
+      drawLine(
+        this.ctx,
+        { x1: x, y1: y - (5 * radius) / 3 },
+        { x2: x, y2: y - radius / 3 }
+      );
+      drawLine(
+        this.ctx,
+        { x1: x, y1: y + (5 * radius) / 3 },
+        { x2: x, y2: y + radius / 3 }
+      );
+    },
+    drawShip() {
+      const { x, y, height, width } = this.ship;
+      const x0 = 0;
+      const y0 = 0;
+      const x1 = height;
+      const y1 = -width / 2;
+      const x2 = (2 * height) / 3;
+      const y2 = 0;
+      const x3 = height;
+      const y3 = width / 2;
+
+      const dx = x - this.mouse.x;
+      const dy = y - this.mouse.y;
+      const theta = Math.atan2(dy, dx);
+
+      const v0 = coordinateOfPointAfterRotation(x0, y0, theta);
+      const v1 = coordinateOfPointAfterRotation(x1, y1, theta);
+      const v2 = coordinateOfPointAfterRotation(x2, y2, theta);
+      const v3 = coordinateOfPointAfterRotation(x3, y3, theta);
+
+      drawLine(
+        this.ctx,
+        { x1: x + v0.x, y1: y + v0.y },
+        { x2: x + v1.x, y2: y + v1.y }
+      );
+      drawLine(
+        this.ctx,
+        { x1: x + v1.x, y1: y + v1.y },
+        { x2: x + v2.x, y2: y + v2.y }
+      );
+      drawLine(
+        this.ctx,
+        { x1: x + v2.x, y1: y + v2.y },
+        { x2: x + v3.x, y2: y + v3.y }
+      );
+      drawLine(
+        this.ctx,
+        { x1: x + v3.x, y1: y + v3.y },
+        { x2: x + v0.x, y2: y + v0.y }
+      );
     },
   },
 };
